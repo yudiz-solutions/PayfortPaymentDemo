22033ba406882bc357a94d941424e42856810c50
yudiz_kevin <kevin.s@yudiz.in>
2018-10-02 11:50:03 +0530

    Initial Commit

---
+++ PayfortPaymentDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayfortPaymentDemo
+//
+//  Created by Yudiz on 02/10/18.
+//  Copyright © 2018 Yudiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
